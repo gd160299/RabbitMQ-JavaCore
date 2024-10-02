fc131cf3eba484a3395671ebbae427d3197bb35a
Do Hoang Giang <gd160299@gmail.com>
2024-10-02 17:07:24 +0700

    feat(): add config external

--- src/main/java/com/pj/utils/ConnectionUtil.java
+++ src/main/java/com/pj/utils/ConnectionUtil.java
@@ -3,8 +3,26 @@ package com.pj.utils;
 import com.pj.config.RabbitMqConfig;
 import com.rabbitmq.client.Connection;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class ConnectionUtil {
-    private static final RabbitMqConfig pool = new RabbitMqConfig(10, "localhost");
+    private static final int maxConnections;
+    private static final String host;
+
+    static {
+        Properties properties = new Properties();
+        try (FileInputStream input = new FileInputStream("./config/config.properties")) {
+            properties.load(input);
+            maxConnections = Integer.parseInt(properties.getProperty("rabbitmq.max.connections", "10"));
+            host = properties.getProperty("rabbitmq.host", "localhost");
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load properties file", e);
+        }
+    }
+
+    private static final RabbitMqConfig pool = new RabbitMqConfig(maxConnections, host);
 
     public static Connection getConnection() throws InterruptedException {
         return pool.getConnection();
